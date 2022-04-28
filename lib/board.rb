39844baf4db035c348458aa3e4aebc15811fec29
KarineDHoshi <103076747+KarineDHoshi@users.noreply.github.com>
2022-04-28 16:42:25 +0200

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -39,7 +39,14 @@ class Board
    end
    
    def victory?
-    if @board[0] == @board[1] && @board[0] == @board[2] && @board[0] != "."  || @board[3] == @board[4] && @board[3] == @board[5] && @board[3] != "."  || @board[6] == @board[7] && @board[6] == @board[8] && @board[6] != "."  || @board[0] == @board[3] && @board[0] == @board[6] && @board[0] != "."  || @board[1] == @board[4] && @board[1] == @board[7] && @board[1] != "."  || @board[2] == @board[5] && @board[2] == @board[8] && @board[2] != "."  || @board[0] == @board[4] && @board[0] == @board[8] && @board[0] != "."  || @board[2] == @board[4] && @board[2] == @board[6] && @board[2] != "." 
+    if @board[0] == @board[1] && @board[0] == @board[2] && @board[0] != "."  ||
+       @board[3] == @board[4] && @board[3] == @board[5] && @board[3] != "."  || 
+       @board[6] == @board[7] && @board[6] == @board[8] && @board[6] != "."  || 
+       @board[0] == @board[3] && @board[0] == @board[6] && @board[0] != "."  || 
+       @board[1] == @board[4] && @board[1] == @board[7] && @board[1] != "."  || 
+       @board[2] == @board[5] && @board[2] == @board[8] && @board[2] != "."  || 
+       @board[0] == @board[4] && @board[0] == @board[8] && @board[0] != "."  || 
+       @board[2] == @board[4] && @board[2] == @board[6] && @board[2] != "." 
       return true
     else
       return false
